3078bc047542875e4ebe2b1cceb9c2693dd94cc5
Ryan Chambers <ryan.b.chambers@gmail.com>
2011-10-31 19:52:09 -0400

    first commit

---
+++ README.markdown
@@ -0,0 +1,23 @@
+JS Test Driver Sample Project
+=============================
+
+This set up should work both from the maven command line and using the eclipse
+JS Test Driver plug-in.
+
+Maven
+-----
+
+I've hard-coded the paths to firefox and internet explorer. I had problems getting Chrome to work
+so it's commented out. I think you can just keep the name of the executable files if the browsers are
+in your path.
+
+mvn clean install
+
+Eclipse
+-------
+
+Install the [eclipse plug-in](http://code.google.com/p/js-test-driver/wiki/UsingTheEclipsePlugin).
+This set up works with version 1.1.1c of the plug-in.
+
+You'll need to create a new JS Test Driver Test run configuration. Pick the jstestdriver-sample-project
+project and pick jsTestDriver.conf as the config file.
---
+++ src/test/js/validation_test.js
@@ -0,0 +1,84 @@
+TestCase("validateAlphaNumeric", {
+	"test validateAlphaNumeric valid" : function() {
+        /*:DOC input = <input type="text" id="test_field" value="aB1" /> */
+		var field = $(this.input),
+			isValid;
+		isValid = validation.validateAlphaNumeric(field);
+		assertEquals("aB1 should be valid", true, isValid);
+	},
+	"test validateAlphaNumeric invalid" : function() {
+        /*:DOC input = <input type="text" id="test_field" value="!" /> */
+		var field = $(this.input),
+			isValid;
+		isValid = validation.validateAlphaNumeric(field);
+		assertEquals("! should not be a valid alphanumeric value", false, isValid);
+	}
+});
+
+function createFakeResponse(responseCode) {
+    return [
+	 	200,
+	 	{ "Content-Type": "text/xml" },
+	 	'<?xml version="1.0" encoding="UTF-8"?><username>' + responseCode + '</username>'
+	];
+}
+
+function callValidation(field, server) {
+	validation.validateUsername(field);
+
+	server.respond();
+
+	return validation.validateAlphaNumeric(field);
+}
+
+TestCase("validateUsername", sinon.testCase({
+	setUp : function() {
+		validation.reset();
+	    this.server = sinon.fakeServerWithClock.create();
+	    /*:DOC input = <input type="text" id="test_field" value="!" /> */
+	},
+	tearDown: function() {
+	    this.server.restore();
+	},
+	"test validateUsername user name already used" : function() {
+		var field = $(this.input),
+			isValid;
+
+		this.server.respondWith(createFakeResponse('error : username already used'));
+
+		isValid = callValidation(field, this.server);
+
+		assertEquals("should have got username already used error", isValid, false);
+	},
+	"test validateUsername user name contains invalid characters" : function() {
+		var field = $(this.input),
+			isValid;
+
+		this.server.respondWith(createFakeResponse('error : username contains invalid characters'));
+
+		isValid = callValidation(field, this.server);
+
+		assertEquals("should have got username contains invalid characters error", isValid, false);
+	},
+	"test validateUsername user name contains spaces" : function() {
+		var field = $(this.input),
+			isValid;
+
+		this.server.respondWith(createFakeResponse('error : username contains spaces'));
+
+		isValid = callValidation(field, this.server);
+
+		assertEquals("should have got username has spaces error", isValid, false);
+	},
+	"test validateUsername valid" : function() {
+        /*:DOC input = <input type="text" id="test_field" value="chuck_norris" /> */
+		var field = $(this.input),
+			isValid;
+
+		this.server.respondWith(createFakeResponse('valid'));
+
+		isValid = callValidation(field, this.server);
+
+		assertEquals("username should be valid", isValid, true);
+	}
+}));
